70149fca0acdefd1414bd7dd6e2839d7163c39c8
KovOlga <switch.to.olga@yandex.ru>
2022-12-18 20:41:29 +0300

    added changable mode

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
-const slider = document.querySelector(".switch");
+// const userDarkMode = localStorage.getItem("darkMode");
+const switchBtn = document.querySelector(".switch");
 
 function changeDarkMode() {
-	console.log(slider.checked);
-	if (slider.checked) {
+	if (switchBtn.checked) {
+		console.log(switchBtn.checked);
+		document.body.classList.add("darkMode");
 	} else {
+		document.body.classList.remove("darkMode");
 	}
 }
 
-slider.addEventListener("change", changeDarkMode());
+switchBtn.addEventListener("change", changeDarkMode);
