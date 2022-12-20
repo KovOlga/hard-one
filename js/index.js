b3810116cdf952c7dc2ecb40dfec48050966f7b0
KovOlga <switch.to.olga@yandex.ru>
2022-12-20 17:57:15 +0300

    added custom pagination

--- js/index.js
+++ js/index.js
@@ -12,20 +12,18 @@ function changeDarkMode() {
 
 switchBtn.addEventListener("change", changeDarkMode);
 
+const menu = ["Шоссе", "Грэвел", "ТТ"];
+
 const swiperIntro = new Swiper(".swiper__types", {
 	loop: true,
-	// pagination: {
-	// 	el: ".swiper__pagination_intro",
-	// 	bulletClass: "swiper-pagination-bullet swiper-pagination-intro-bullet",
-	// 	bulletActiveClass: "swiper-pagination-intro-bullet-active",
-	// },
+	pagination: {
+		el: ".swiper-pagination",
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + menu[index] + "</span>";
+		},
+	},
 	// navigation: {
 	// 	nextEl: ".swiper__switch-btn_intro_next",
 	// 	prevEl: ".swiper__switch-btn_intro_prev",
-	// },
-	// autoplay: {
-	// 	delay: 10000,
-	// 	stopOnLastSlide: false,
-	// 	disableOnInteraction: false,
 	// },
 });
