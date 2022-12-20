8efe5e7fda0efbc108977015bb18fce649f16e3b
KovOlga <switch.to.olga@yandex.ru>
2022-12-20 17:04:32 +0300

    added three slides and swiper

--- js/index.js
+++ js/index.js
@@ -11,3 +11,21 @@ function changeDarkMode() {
 }
 
 switchBtn.addEventListener("change", changeDarkMode);
+
+const swiperIntro = new Swiper(".swiper__types", {
+	loop: true,
+	// pagination: {
+	// 	el: ".swiper__pagination_intro",
+	// 	bulletClass: "swiper-pagination-bullet swiper-pagination-intro-bullet",
+	// 	bulletActiveClass: "swiper-pagination-intro-bullet-active",
+	// },
+	// navigation: {
+	// 	nextEl: ".swiper__switch-btn_intro_next",
+	// 	prevEl: ".swiper__switch-btn_intro_prev",
+	// },
+	// autoplay: {
+	// 	delay: 10000,
+	// 	stopOnLastSlide: false,
+	// 	disableOnInteraction: false,
+	// },
+});
