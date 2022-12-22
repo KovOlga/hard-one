da3d71b8f88b794f8b6b4118a6c3d91cdf9641a8
KovOlga <switch.to.olga@yandex.ru>
2022-12-22 14:53:24 +0300

    fixed mode in js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 // const userDarkMode = localStorage.getItem("darkMode");
-const switchBtn = document.querySelector(".switch");
+const switchBtn = document.querySelector(".mode__checkbox");
 
 function changeDarkMode() {
 	if (switchBtn.checked) {
-		console.log(switchBtn.checked);
 		document.body.classList.add("darkMode");
 	} else {
 		document.body.classList.remove("darkMode");
